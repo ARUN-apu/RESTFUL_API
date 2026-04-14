b2fc3608e1b0169b9b314c947ee1b04bf1de4bbd
Arun Senapati <arunsenapati407@gmail.com>
2026-04-14 14:49:44 +0530

    Inital Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+let posts = [
+    {
+        username: "Arun",
+        content: "I love coding!"
+    },
+    {
+        username: "Apu",
+        content: "Hi"
+    },
+    {
+        username: "Arun Senapati",
+        content: "Hard work is important to achieve success"
+    }
+];
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { posts });
+})
+
+app.listen(port, () => {
+    console.log("listening to port : 8080");
+})
